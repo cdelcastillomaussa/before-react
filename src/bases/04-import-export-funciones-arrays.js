3546d890253e610a878f86c0b128daedb3653ebd
Carlos Del Castillo M <maussac13@gmail.com>
2024-06-18 21:35:17 -0500

    promises

--- src/bases/04-import-export-funciones-arrays.js
+++ src/bases/04-import-export-funciones-arrays.js
@@ -1,14 +1,14 @@
 import heroes, { owners } from "../data/heroes";
-console.log(heroes);
-console.log(owners);
+// console.log(owners);
+// console.log(heroes);
 
-const getHeroById = (id) => {
+export const getHeroById = (id) => {
   return heroes.find((element) => element.id === id);
 };
-console.log(getHeroById(2));
+// console.log(getHeroById(2));
 
 const getHeroesByOwner = (owner) => {
   return heroes.filter((item) => item.owner === owner);
 };
 
-console.log(getHeroesByOwner("Marvel"));
+// console.log(getHeroesByOwner("Marvel"));
