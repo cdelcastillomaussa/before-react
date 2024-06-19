91617efacd2507a31059cc3cf3d4857707ebd04c
Carlos Del Castillo M <maussac13@gmail.com>
2024-06-18 22:29:20 -0500

    fetch api giphy

---
+++ src/bases/fetch api.js
@@ -0,0 +1,13 @@
+const apiKey = "lD6RAnjfVNQnhrWgBQRrUUSvNixj8Wb6";
+
+const Getgiphy = fetch(
+  `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+);
+Getgiphy.then((res) => res.json())
+  .then(({ data }) => {
+    const { url } = data.images.original;
+    const img = document.createElement("img");
+    img.src = url;
+    document.body.append(img);
+  })
+  .catch((error) => console.error(error));
